6a29b8f71e222f7be69f0e350329bb209a94b2a8
Yan Rodrigues <ccyan05@alu.ufc.br>
2022-01-11 10:57:18 -0300

    Nvim essential files

---
+++ nvim/lua/user/keymaps.lua
@@ -0,0 +1,45 @@
+local opts = { noremap = true, silent = true }
+
+local term_opts = { silent = true }
+
+-- Shorten function name
+local keymap = vim.api.nvim_set_keymap
+
+--Remap space as leader key
+-- keymap("", "<C-a>", "<Nop>", opts)
+vim.g.mapleader = "#"
+-- vim.g.maplocalleader = "<C-a>"
+
+-- Window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- Explore
+keymap("n", "<leader>e", ":NvimTreeToggle<cr>", opts)
+
+-- Navigate buffers
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
+
+-- Visual --
+-- Stay in indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- Move text up and down
+keymap("v", "<A-j>", ":m .+1<CR>==", opts)
+keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+
+-- Telescope
+keymap("n", "<leader>f", ": Telescope find_files<cr>", opts)
+keymap("n", "<leader>g", ": Telescope live_grep<cr>", opts)
+keymap("n", "<leader>b", ": Telescope buffers<cr>", opts)
+
+-- Resize with arrows
+keymap("n", "<C-Up>", ":resize -2<CR>", opts)
+keymap("n", "<C-Down>", ":resize +2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
