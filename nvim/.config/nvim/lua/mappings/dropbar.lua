65193a29c9c45fef41b63bdf8ce057ffb66ccd4b
Yan Rodrigues <yan.rodriguescs@outlook.com>
2026-08-03 20:44:46 -0300

    fix(mappings): defer plugin requires in keymap callbacks

--- nvim/.config/nvim/lua/mappings/dropbar.lua
+++ nvim/.config/nvim/lua/mappings/dropbar.lua
@@ -1,3 +1,5 @@
 local map = vim.keymap.set
 
-map("n", "<leader>;", require("dropbar.api").pick, { desc = "Pick symbols in winbar" })
+map("n", "<leader>;", function()
+  require("dropbar.api").pick()
+end, { desc = "Pick symbols in winbar" })
--- nvim/.config/nvim/lua/mappings/ufo.lua
+++ nvim/.config/nvim/lua/mappings/ufo.lua
@@ -1,4 +1,9 @@
 local map = vim.keymap.set
 
-map("n", "zR", require("ufo").openAllFolds, { desc = "Open all folds" })
-map("n", "zM", require("ufo").closeAllFolds, { desc = "Close all folds" })
+map("n", "zR", function()
+  require("ufo").openAllFolds()
+end, { desc = "Open all folds" })
+
+map("n", "zM", function()
+  require("ufo").closeAllFolds()
+end, { desc = "Close all folds" })
